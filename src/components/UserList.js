f868423f50e70604430b8a1bd83f00e02ddf3e6e
Anil Kumar <mak97047@gmail.com>
2025-01-28 11:18:33 +0530

    fixed the white space issue in responsiveness.

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import './UserList.css';
 import { FiEdit, FiTrash2 } from "react-icons/fi";
+
 const UserList = ({ users, onEditUser, onDeleteUser }) => (
   <div className="user-list">
     <h2>Users List</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => (
-          <tr  className='user-details' key={user.id}>
-            <td style={{textAlign:"center"}}>{user.customId || user.id}</td>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td style={{display:"flex", justifyContent:"center"}}>
-              <button onClick={() => onEditUser(user)} className="edit-btn action-btn"><FiEdit/></button>
-              <button onClick={() => onDeleteUser(user.id)} className="delete-btn action-btn"><FiTrash2/></button>
-            </td>
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Actions</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr className='user-details' key={user.id}>
+              <td style={{textAlign:"center"}}>{user.customId || user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td style={{display:"flex", justifyContent:"center"}}>
+                <button onClick={() => onEditUser(user)} className="edit-btn action-btn">
+                  <FiEdit/>
+                </button>
+                <button onClick={() => onDeleteUser(user.id)} className="delete-btn action-btn">
+                  <FiTrash2/>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   </div>
 );
 
